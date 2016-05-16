1eb595eefc3c36be9669eb9218e75234b5c2be88
hitman <hitman@quartsoft.com>
2016-05-16 12:08:09 +0300

    fixes

--- blueprints/professor.py
+++ blueprints/professor.py
@@ -1,19 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from flask.ext.login import current_user
 from flask_login import login_required
-from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename, redirect
 
 from database import db
 from forms.edit_professor import EditProfessorForm
 from models.professor import Professor
-from config import Config
 
 professor = Blueprint('professor', __name__, template_folder='templates')
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in Config.ALLOWED_EXTENSIONS
 
 
 @login_required
@@ -28,16 +22,6 @@ def personal_page():
 def edit():
     person = Professor.query.get_or_404(current_user.get_id())
     form = EditProfessorForm()
-    form.name.data = person.user.name
-    form.second_name.data = person.user.second_name
-    form.middle_name.data = person.user.middle_name
-    form.birthday.data = person.user.birthday
-    form.email.data = person.user.email
-    form.facility.data = person.facility
-    form.rank.data = person.rank
-    form.academic_degree.data = person.academic_degree
-    form.photo.data = person.photo
-    form.post.data = person.post
     if form.is_submitted():
         filename = secure_filename(form.photo.data.filename)
         photo = 'static/photos/user_' + str(person.id) + '_' + filename
@@ -45,4 +29,16 @@ def edit():
         person.photo = 'photos/user_' + str(person.id) + '_' + filename
         db.session.add(person)
         db.session.commit()
+        return redirect(url_for('professor.personal_page'))
+    else:
+        form.name.data = person.user.name
+        form.second_name.data = person.user.second_name
+        form.middle_name.data = person.user.middle_name
+        form.birthday.data = person.user.birthday
+        form.email.data = person.user.email
+        form.facility.data = person.facility
+        form.rank.data = person.rank
+        form.academic_degree.data = person.academic_degree
+        form.post.data = person.post
+        form.photo.data = person.photo
     return render_template('professor/edit/edit.html', form=form)
