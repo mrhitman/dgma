4194a295a20236aeafd572075c2253ce04141d50
hitman <hitman@quartsoft.com>
2016-05-18 11:03:06 +0300

    add cathedras

--- blueprints/facility.py
+++ blueprints/facility.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 from models.cathedra import Cathedra
 from models.facility import Facility
+from models.professor import Professor
 
 facility = Blueprint('facility', __name__, template_folder='templates')
 
@@ -16,3 +17,9 @@ def index():
 def cathedras(id):
     cathedras = Cathedra.query.filter_by(facility_id=id).all()
     return render_template('facility/cathedras/cathedras.html', cathedras=cathedras)
+
+
+@facility.route('/professors/<int:id>')
+def professors(id):
+    professors = Professor.query.filter_by(cathedra_id=id).all()
+    return render_template('facility/professors/professors.html', professors=professors)
