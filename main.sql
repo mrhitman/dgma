5242f67db7de281a0a15b777c582382abf094cba
hitman <hitman@quartsoft.com>
2016-05-18 11:04:56 +0300

    add sql

--- main.sql
+++ main.sql
@@ -1,7 +1,7 @@
 /*
 Navicat SQLite Data Transfer
 
-Source Server         : lllll
+Source Server         : data
 Source Server Version : 30714
 Source Host           : :0
 
@@ -9,10 +9,30 @@ Target Server Type    : SQLite
 Target Server Version : 30714
 File Encoding         : 65001
 
-Date: 2016-05-17 21:08:50
+Date: 2016-05-18 11:04:35
 */
 
 PRAGMA foreign_keys = OFF;
+
+-- ----------------------------
+-- Table structure for facility
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."facility";
+CREATE TABLE "facility" (
+"id"  INTEGER NOT NULL,
+"name"  VARCHAR(50),
+"short_name"  VARCHAR(15),
+"image"  VARCHAR(100),
+PRIMARY KEY ("id" ASC)
+);
+
+-- ----------------------------
+-- Records of facility
+-- ----------------------------
+INSERT INTO "main"."facility" VALUES (1, 'ФАКУЛЬТЕТ АВТОМАТИЗАЦИИ МАШИНОСТРОЕНИЯ И ИНФОРМАЦИОННЫХ ТЕХНОЛОГИЙ ', 'ФАМИТ', 'facility/famit.jpg');
+INSERT INTO "main"."facility" VALUES (2, 'ФАКУЛЬТЕТ МАШИНОСТРОЕНИЯ ', 'ФМ', 'facility/finansi.jpg');
+INSERT INTO "main"."facility" VALUES (3, 'ФАКУЛЬТЕТ ИНТЕГРИРОВАННЫХ ТЕХНОЛОГИЙ И ОБОРУДОВАНИЯ', 'ФИТО', 'facility/fito.jpg');
+INSERT INTO "main"."facility" VALUES (4, 'ФАКУЛЬТЕТ ЭКОНОМИКИ И МЕНЕДЖМЕНТА', 'ФЭМ', 'facility/fm.jpg');
 
 -- ----------------------------
 -- Table structure for cathedra
@@ -31,96 +51,29 @@ CREATE TABLE cathedra (
 -- ----------------------------
 -- Records of cathedra
 -- ----------------------------
-INSERT INTO "main"."cathedra" VALUES (1, null, 'АПП', null, 1);
-INSERT INTO "main"."cathedra" VALUES (2, null, 'ИСПР', null, 1);
-INSERT INTO "main"."cathedra" VALUES (3, null, 'ЭСА', null, 1);
-INSERT INTO "main"."cathedra" VALUES (4, null, 'КИТ', null, 1);
-INSERT INTO "main"."cathedra" VALUES (5, null, 'ИиИг', null, 1);
-INSERT INTO "main"."cathedra" VALUES (6, null, 'ТМ', null, 1);
-INSERT INTO "main"."cathedra" VALUES (7, null, 'АММО', null, 2);
-INSERT INTO "main"."cathedra" VALUES (8, null, 'ПТМ', null, 2);
-INSERT INTO "main"."cathedra" VALUES (9, null, 'КМСИиТ', null, 2);
-INSERT INTO "main"."cathedra" VALUES (10, null, 'ВМ', null, 2);
-INSERT INTO "main"."cathedra" VALUES (11, null, 'ИКГ', null, 2);
-INSERT INTO "main"."cathedra" VALUES (12, null, 'КФ', null, 2);
-INSERT INTO "main"."cathedra" VALUES (13, null, 'ТМ', null, 3);
-INSERT INTO "main"."cathedra" VALUES (14, null, 'ОиТПС', null, 3);
-INSERT INTO "main"."cathedra" VALUES (15, null, 'ОМД', null, 3);
-INSERT INTO "main"."cathedra" VALUES (16, null, 'ТОЛП', null, 3);
-INSERT INTO "main"."cathedra" VALUES (17, null, 'ХиОТ', null, 3);
-INSERT INTO "main"."cathedra" VALUES (18, null, 'ОПМ', null, 3);
-INSERT INTO "main"."cathedra" VALUES (19, null, 'Финансы', null, 4);
-INSERT INTO "main"."cathedra" VALUES (20, null, 'УиА', null, 4);
-INSERT INTO "main"."cathedra" VALUES (21, null, 'Менеджмент', null, 4);
-INSERT INTO "main"."cathedra" VALUES (22, null, 'Эп', null, 4);
-INSERT INTO "main"."cathedra" VALUES (23, null, 'МПФ', null, 4);
-INSERT INTO "main"."cathedra" VALUES (24, null, 'ФВ', null, 4);
-INSERT INTO "main"."cathedra" VALUES (25, null, 'ЭТ', null, null);
-
--- ----------------------------
--- Table structure for facility
--- ----------------------------
-DROP TABLE IF EXISTS "main"."facility";
-CREATE TABLE "facility" (
-"id"  INTEGER NOT NULL,
-"name"  VARCHAR(50),
-"short_name"  VARCHAR(15),
-"image"  VARCHAR(100),
-PRIMARY KEY ("id" ASC)
-);
-
--- ----------------------------
--- Records of facility
--- ----------------------------
-INSERT INTO "main"."facility" VALUES (1, 'ФАКУЛЬТЕТ АВТОМАТИЗАЦИИ МАШИНОСТРОЕНИЯ И ИНФОРМАЦИОННЫХ ТЕХНОЛОГИЙ ', 'ФАМИТ', null);
-INSERT INTO "main"."facility" VALUES (2, 'ФАКУЛЬТЕТ МАШИНОСТРОЕНИЯ ', 'ФМ', null);
-INSERT INTO "main"."facility" VALUES (3, 'ФАКУЛЬТЕТ ИНТЕГРИРОВАННЫХ ТЕХНОЛОГИЙ И ОБОРУДОВАНИЯ', 'ФИТО', null);
-INSERT INTO "main"."facility" VALUES (4, 'ФАКУЛЬТЕТ ЭКОНОМИКИ И МЕНЕДЖМЕНТА', 'ФЭМ', null);
-
--- ----------------------------
--- Table structure for professor
--- ----------------------------
-DROP TABLE IF EXISTS "main"."professor";
-CREATE TABLE professor (
-	id INTEGER NOT NULL, 
-	user_id INTEGER, 
-	cathedra_id INTEGER, 
-	post VARCHAR(50), 
-	academic_degree VARCHAR(50), 
-	rank VARCHAR(50), 
-	photo VARCHAR(100), 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(user_id) REFERENCES user (id), 
-	FOREIGN KEY(cathedra_id) REFERENCES cathedra (id)
-);
-
--- ----------------------------
--- Records of professor
--- ----------------------------
-INSERT INTO "main"."professor" VALUES (1, 1, 2, 'Преподаватель', 1, 'Старший преподаватель', null);
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS "main"."user";
-CREATE TABLE user (
-	id INTEGER NOT NULL, 
-	name VARCHAR(50), 
-	second_name VARCHAR(50), 
-	middle_name VARCHAR(50), 
-	password VARCHAR(50), 
-	email VARCHAR(50), 
-	birthday DATETIME, 
-	PRIMARY KEY (id)
-);
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO "main"."user" VALUES (1, 'Виктор', 'Вареник', 'Владимирович', 1, 'kabalx47@gmail.com', '1988-12-25 00:00:00.000000');
-
--- ----------------------------
--- Indexes structure for table user
--- ----------------------------
-CREATE UNIQUE INDEX "main"."ix_user_email"
-ON "user" ("email" ASC);
+INSERT INTO "main"."cathedra" VALUES (1, null, 'АПП', 'cathedra/app.jpg', 1);
+INSERT INTO "main"."cathedra" VALUES (2, null, 'ИСПР', 'cathedra/ispr.jpg', 1);
+INSERT INTO "main"."cathedra" VALUES (3, null, 'ЭСА', 'cathedra/esa.jpg', 1);
+INSERT INTO "main"."cathedra" VALUES (4, null, 'КИТ', 'cathedra/it.jpg', 1);
+INSERT INTO "main"."cathedra" VALUES (5, null, 'ИиИг', 'cathedra/iiig.jpg', 1);
+INSERT INTO "main"."cathedra" VALUES (6, null, 'ТМ', 'cathedra/tm.jpg', 1);
+INSERT INTO "main"."cathedra" VALUES (7, null, 'АММО', 'cathedra/ammio.jpg', 2);
+INSERT INTO "main"."cathedra" VALUES (8, null, 'ПТМ', 'cathedra/ptm.jpg', 2);
+INSERT INTO "main"."cathedra" VALUES (9, null, 'КМСИиТ', 'cathedra/msi.jpg', 2);
+INSERT INTO "main"."cathedra" VALUES (10, null, 'ВМ', 'cathedra/vm.jpg', 2);
+INSERT INTO "main"."cathedra" VALUES (11, null, 'ИГ', 'cathedra/graf.jpg', 2);
+INSERT INTO "main"."cathedra" VALUES (12, null, 'КФ', 'cathedra/fiz.jpg', 2);
+INSERT INTO "main"."cathedra" VALUES (13, null, 'ТМ', 'cathedra/tm_1.jpg', 3);
+INSERT INTO "main"."cathedra" VALUES (14, null, 'ОиТПС', 'cathedra/sp.jpg', 3);
+INSERT INTO "main"."cathedra" VALUES (15, null, 'ОМД', 'cathedra/omd.jpg', 3);
+INSERT INTO "main"."cathedra" VALUES (16, null, 'ТОЛП', 'cathedra/lp.jpg', 3);
+INSERT INTO "main"."cathedra" VALUES (17, null, 'ХиОТ', 'cathedra/hiop.jpg', 3);
+INSERT INTO "main"."cathedra" VALUES (18, null, 'ОПМ', 'cathedra/opm.jpg', 3);
+INSERT INTO "main"."cathedra" VALUES (19, null, 'Финансы', 'cathedra/f.jpg', 4);
+INSERT INTO "main"."cathedra" VALUES (20, null, 'УиА', 'cathedra/yia.jpg', 4);
+INSERT INTO "main"."cathedra" VALUES (21, null, 'Менеджмент', 'cathedra/logo_kaf.png', 4);
+INSERT INTO "main"."cathedra" VALUES (22, null, 'Эп', 'cathedra/ep.jpg', 4);
+INSERT INTO "main"."cathedra" VALUES (23, null, 'МПФ', 'cathedra/eng.jpg', 4);
+INSERT INTO "main"."cathedra" VALUES (24, null, 'ФВ', 'cathedra/fv.jpg', 4);
+INSERT INTO "main"."cathedra" VALUES (25, null, 'ЭТ', 'cathedra/et.jpg', 4);
+INSERT INTO "main"."cathedra" VALUES (26, null, 'Философия', 'cathedra/fil.jpg', 4);
